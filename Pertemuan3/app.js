3bab7a585781125ce38b321afe9a54b7e47f484b
RizkyPratama05 <rizkypp428@gmail.com>
2025-08-12 10:17:08 +0700

    connect database dan view data

--- Pertemuan3/app.js
+++ Pertemuan3/app.js
@@ -1,18 +1,19 @@
-const express = require('express');
-require("dotenv").config();
+const express = require("express");
+require('dotenv').config({ path: __dirname + '/.env' });
 const app = express();
 // const port = 3000;
-const todoRoutes = require("./routes/todo.js");
+const todoRoutes = require("./routes/tododb.js");
 const { todos } = require("./routes/todo.js");
+const db = require("./Database/db.js");
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/todos', todoRoutes);
+app.use("/todos", todoRoutes);
 
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.render("index");
 });
 
@@ -28,12 +29,19 @@ app.get("/todos-list", (req, res) => {
     res.render("todos-page", { todos: todos });
 });
 
+app.get("/todo-view", (req, res) => {
+    db.query("SELECT * FROM todos", (err, todos) => {
+        if (err) return res.status(500).send("Internal Server Error");
+        res.render("todo", { todos: todos });
+    });
+});
+
 app.use((req, res) => {
     res.status(404).send("404 - Page Not Found");
 });
 
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 
 
 
---
+++ Pertemuan3/prak6.md
@@ -0,0 +1,3 @@
+![alt text](<Screenshot 2025-08-12 101434.png>)
+![alt text](<Screenshot 2025-08-12 101523.png>)
+![alt text](<Screenshot 2025-08-12 101556.png>)
